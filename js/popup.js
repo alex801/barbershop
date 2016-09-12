acd1ddacc96dd47d598890e71570e114175d7e84
Сергей < @DMITRYEV>
2016-09-12 09:36:11 +0300

    Добавил скрипт появления карты

--- js/popup.js
+++ js/popup.js
@@ -5,6 +5,9 @@ var form = popup.querySelector('form');
 var login = popup.querySelector('[name=login]');
 var password = popup.querySelector('[name=password]');
 var storage = localStorage.getItem('login');
+var mapButton = document.querySelector('.map');
+var map = document.querySelector('.modal-content-map');
+var mapClose = map.querySelector('.modal-content-close');
 
 link.addEventListener('click', function(event) {
   event.preventDefault();
@@ -38,6 +41,24 @@ window.addEventListener('keydown', function(event) {
     if (popup.classList.contains('modal-content-show')) {
       popup.classList.remove('modal-content-show');
       popup.classList.remove('modal-error');
+    }
+  }
+});
+
+mapButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    map.classList.add('modal-content-map-show');
+});
+
+mapClose.addEventListener('click', function(event) {
+    event.preventDefault();
+    map.classList.remove('modal-content-map-show');
+});
+
+window.addEventListener('keydown', function(event) {
+  if (event.keyCode === 27) {
+    if (map.classList.contains('modal-content-map-show')) {
+      map.classList.remove('modal-content-map-show');
     }
   }
 });
